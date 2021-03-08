0b222ab759399e610d4dab8cc431c8f62816d7be
FishExploitsYT <72241830+FishExploitsYT@users.noreply.github.com>
2021-03-09 09:14:33 +1000

    Create Ugly code aimbot but good.lua

---
+++ Ugly code aimbot but good.lua
@@ -0,0 +1,7 @@
+--press rightmouse to aim
+--to change options just do
+--Options.Enabled = false (to disable)
+--Options.Smoothness = false (to disable smoothness)
+-----------------------------------------------------------------------------------------
+loadstring(game:HttpGet("https://pastebin.com/raw/QWyqDv7u"))()
+Options.Smoothness = false
